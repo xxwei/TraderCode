e3c6cedb09e6cf9f6c167d02ce5b7a61105b5eac
alex.chai <chaixiaowei2008@qq.com>
2017-06-23 00:09:49 +0800

    keras's callback for tensorboard

--- KerasRnn.py
+++ KerasRnn.py
@@ -39,11 +39,12 @@ data_dim = 16
 timesteps = 8
 num_classes = 10
 batch_size = 32
-
+tbCallBack  = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0,write_graph=True, write_images=True)
 # Expected input batch shape: (batch_size, timesteps, data_dim)
 # Note that we have to provide the full batch_input_shape since the network is stateful.
 # the sample of index i in batch k is the follow-up for the sample i in batch k-1.
 model = Sequential()
+#tbCallBack.set_model(model)
 model.add(LSTM(32, return_sequences=True, stateful=True,
                batch_input_shape=(batch_size, timesteps, data_dim)))
 model.add(LSTM(32, return_sequences=True, stateful=True))
@@ -64,6 +65,7 @@ y_val = np.random.random((batch_size * 3, num_classes))
 
 model.fit(x_train, y_train,
           batch_size=batch_size, epochs=5, shuffle=False,
-          validation_data=(x_val, y_val))
+          validation_data=(x_val, y_val),callbacks=[tbCallBack])
 
 model.evaluate(x_val,y_val,batch_size=batch_size)
+
