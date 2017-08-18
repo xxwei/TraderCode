5a6dd1d4178d6f82cd68a158809ce4fd9712197c
xiaowei.chai <chai.xiaowei@whaley.cn>
2017-08-18 22:24:47 +0800

    add R file

---
+++ Test.R
@@ -0,0 +1,36 @@
+# Create a vector.
+apple<-c('red','green','yellow')
+print(class(apple))
+# Create a matrix.
+M = matrix( c('a','a','b','c','b','a'), nrow = 2, ncol = 3, byrow = TRUE)
+print(M)
+# Create an array.
+a <- array(c('green','yellow'),dim = c(3,3,2))
+print(a)
+# Create a list.
+list1 <- list(c(2,5,3),21.3,sin)
+print(list1)
+
+# Create a vector.
+apple_colors <- c('green','green','yellow','red','red','red','green')
+
+# Create a factor object.
+factor_apple <- factor(apple_colors)
+
+# Print the factor.
+print(factor_apple)
+print(nlevels(factor_apple))
+
+# Create the data frame.
+BMI <- 	data.frame(
+  gender = c("Male", "Male","Female"), 
+  height = c(152, 171.5, 165), 
+  weight = c(81,93, 78),
+  Age = c(42,38,26)
+)
+print(BMI)
+
+
+require(ggplot2)
+
+ggplot(BMI,aes(x="height",y="weight",color="Age"))+geom_point()
--- plotSample.py
+++ plotSample.py
@@ -1,6 +1,20 @@
 # -*- coding:utf-8 -*-
 from ggplot import *
 
+
+
+p = ggplot(mtcars, aes(x='wt', y='mpg',size='disp',color='qsec')) + \
+    geom_point(shape='D',position = 'jitter')+scale_color_gradient(low='red', high='white')
+print(p)
+
+p = ggplot(aes(x='carat', y='price', color='clarity',size='table'), data=diamonds) +\
+    geom_point() +\
+    scale_color_brewer(type = 'qual', palette = 3)
+
+print(p)
+
+
+
 p = ggplot(mtcars,aes(x='wt',y='mpg')) + \
     geom_point() + \
     geom_abline(intercept=20)
@@ -10,12 +24,6 @@ print(p)
 p =  ggplot(aes(x='date', y='beef', ymin='beef - 1000', ymax='beef + 1000'), data=meat) + \
     geom_area() + \
     geom_point(color='coral')
-
-print(p)
-
-p = ggplot(aes(x='carat', y='price', color='clarity'), data=diamonds) +\
-    geom_point() +\
-    scale_color_brewer(type='qual')
 
 print(p)
 
